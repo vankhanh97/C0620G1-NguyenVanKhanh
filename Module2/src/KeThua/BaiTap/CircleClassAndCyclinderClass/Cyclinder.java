35a1b4b5a8e26cff45cbb2f2ea23af7d46fd0b10
GC0620G1-NguyenVanKhanh <nvankhanh001@gmail.com>
2020-08-11 08:14:42 +0700

    up bai

---
+++ Module2/src/KeThua/BaiTap/CircleClassAndCyclinderClass/Cyclinder.java
@@ -0,0 +1,35 @@
+package KeThua.BaiTap.CircleClassAndCyclinderClass;
+
+public class Cyclinder extends Circle {
+    private double height = 1.0;
+
+    Cyclinder() {
+    }
+
+    Cyclinder(double radius, String color, double hight) {
+        super(radius, color);
+        this.height = hight;
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return this.height * this.getRadius() * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Volume's Cyclinder " + getVolume() + "\n" +
+                        super.toString();
+
+
+    }
+}
