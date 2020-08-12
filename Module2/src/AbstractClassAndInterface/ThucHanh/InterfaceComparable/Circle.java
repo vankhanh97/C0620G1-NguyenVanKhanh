85e78f7bb4b631c5ee174c08f3512dc32faac207
GC0620G1-NguyenVanKhanh <nvankhanh001@gmail.com>
2020-08-12 08:26:17 +0700

    up bai

--- Module2/src/AbstractClassAndInterface/ThucHanh/InterfaceComparable/Circle.java
+++ Module2/src/AbstractClassAndInterface/ThucHanh/InterfaceComparable/Circle.java
@@ -2,6 +2,24 @@ package AbstractClassAndInterface.ThucHanh.InterfaceComparable;
 
 public class Circle {
     private double radius = 1.0;
+    private String color = "Black";
+    private boolean filled = false;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 
     public Circle() {
     }
@@ -11,8 +29,9 @@ public class Circle {
     }
 
     public Circle(double radius, String color, boolean filled) {
-       
         this.radius = radius;
+        this.color = color;
+        this.filled = filled;
     }
 
     public double getRadius() {
@@ -21,5 +40,11 @@ public class Circle {
 
     public void setRadius(double radius) {
         this.radius = radius;
+    }
+    @Override
+    public String toString(){
+        return   "Radius's Circle "+getRadius()+
+                "\n"+
+                "Color 's Circle "+getColor()+"\n"+ isFilled();
     }
 }
